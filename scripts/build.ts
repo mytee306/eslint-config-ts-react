e575aefe9360978baf31dc79684b45596035d1fe
mateja <mateja176@gmail.com>
2019-04-11 14:30:36 +0200

    Adds build scripts

---
+++ scripts/build.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs"
+import * as path from "path"
+
+const eslintrc = fs.readFileSync(path.join(__dirname, "..", ".eslintrc.json"), {
+  encoding: "UTF-8",
+})
+
+const eslintrcWithExport = `module.exports = ${eslintrc}`
+
+fs.writeFileSync(path.join(__dirname, "..", "index.js"), eslintrcWithExport)
